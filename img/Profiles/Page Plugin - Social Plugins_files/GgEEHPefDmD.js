10627ae9c5557c2adc9e435a3cb5c298da157ee3
Shounak Ray <ray.shounak@gmail.com>
2017-08-01 18:55:10 +0530

    check

--- img/Profiles/Page Plugin - Social Plugins_files/GgEEHPefDmD.js
+++
@@ -1,3 +0,0 @@
-if (self.CavalryLogger) { CavalryLogger.start_js(["Yj+LH"]); }
-
-__d('TabBarShim',['DOMContainer.react','React','isNode'],(function a(b,c,d,e,f,g){var h=function i(j){var k;if(j.children){k=j.children.map(function(m,n){if(typeof m==='object'&&typeof m.ctor==='function'){return i(m);}else if(c('isNode')(m)){var o=c('React').createElement(c('DOMContainer.react'),{key:'TabBarShim-'+n},m);return o;}else return m;});if(k.length===1)k=k[0];}var l=j.ctor;return c('React').createElement(l,j.props,k);};f.exports=h;}),null);
